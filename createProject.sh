473d5fb31d283e8b12cab2286c12d14f6fbe2029
NKay <n.klipp@mailbox.org>
2018-05-24 15:51:28 +0200

    Added Script to create new project

---
+++ createProject.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+  else
+	if [ -z "$1" ]
+	then
+		echo "Empty String as Paramater"
+	else
+		mkdir $1
+		cp -r ./blank/* ./$1
+		mv ./$1/src/blank.h ./$1/src/$1.h 
+		mv ./$1/src/blank.cpp ./$1/src/$1.cpp
+  fi
+	
+fi
