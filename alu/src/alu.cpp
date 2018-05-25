74d83c69724f5bf8706f146041eb7453d16b53f4
NKay <n.klipp@mailbox.org>
2018-05-25 19:14:32 +0200

    ALU and or xor add mul div
    mul dive potentially need better overflow/underflow conditions

--- alu/src/alu.cpp
+++ alu/src/alu.cpp
@@ -30,23 +30,57 @@ case "111": aludiv;
 
 
 void alu::aluand(){
+sc_bv<32> result= (data1.read & data2.read());
+acc.write(result);
 
 }
 void alu::aluor(){
-
+sc_bv<32> result= (data1.read | data2.read());
+acc.write(result);
 }
 void alu::aluxor(){
+	sc_bv<32> result= (data1.read ^ data2.read());
+	acc.write(result);
 
 }
 void alu::aluadd(){
+int int1 = data1.read().to_int();
+int int2 = data2.read().to_int();
+int result = int1 + int2;
+
+acc.write(sc_bv<32>(result));
+if((acc.read() < data1.read()) || (acc.read() < data2.read())){
+	//overflow
+
+}
 
 }
 
 void alu::alumul(){
+	int int1 = data1.read().to_int();
+	int int2 = data2.read().to_int();
+	int result = int1 + int2;
+	sc_bv<32> max;
+	max = "11111111111111111111111111111111";
+
+	acc.write(sc_bv<32>(result));
+	int result2 = acc.read().to_int();
+	if(result >= max.to_int()){
+		//overflow
+
+	}
 
 }
 void alu::aludiv(){
+	int int1 = data1.read().to_int();
+	int int2 = data2.read().to_int();
+	int result = int1 / int2;
 
+	acc.write(sc_bv<32>(result));
+	if((acc.read() > data1.read()) ){
+		//underflow
+
+	}
 }
 void alu::aluror(){
 
