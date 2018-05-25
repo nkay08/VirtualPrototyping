c849f16f57324cdeac127d1e74732566e80484b0
NKay <n.klipp@mailbox.org>
2018-05-25 18:52:23 +0200

    ALU Helper modules

--- alu/src/alu.h
+++ alu/src/alu.h
@@ -46,15 +46,63 @@ SC_MODULE(alu){
 
 };
 
+
+SC_MODULE(resetMod){
+	sc_out<bool> reset;
+	sc_clock clk;
+	bool shallReset;
+
+
+	SC_CTOR(resetMod):clk("clk", sc_time(1, SC_SEC))
+	{
+		SC_METHOD(processing);
+		shallReset = false;
+		sensitive << clk;
+	}
+
+	void processing(){
+
+		reset.write(shallReset);
+
+	}
+
+};
+
+SC_MODULE(opselMod){
+
+	sc_out<sc_bv<3>> op_sel;
+	sc_clock clk;
+	sc_bv<3> sel;
+
+	SC_CTOR(opselMod):clk("clk", sc_time(1, SC_SEC))
+	{
+		SC_METHOD(processing);
+		sensitive << clk;
+
+		sel = "001"
+	}
+
+	void processing(){
+
+		op_sel.write(sel);
+
+	}
+
+}
+
+
 SC_MODULE(drain)
 {
-	sc_in<int> in;
+	sc_in<sc_bv<8>> stat;
+	sc_in<sc_bv<32>> acc;
+	sc_in<sc_bv<32>> data1
+	sc_in<sc_bv<32>> data2;
 
 	SC_HAS_PROCESS(drain);
 	drain(sc_module_name n)
 	{
 		SC_METHOD(processing);
-		sensitive << in;
+		sensitive << acc << stat << data1 << data2;
 		dont_initialize();
 
 	}
@@ -62,7 +110,7 @@ SC_MODULE(drain)
 	void processing()
 	{
 		cout << "Time: " << sc_time_stamp() << endl;
-		cout << "Counter value: " <<  in.read();
+		cout << "Counter value: " <<  acc.read();
 		cout << endl;
 
 	}
