69c11db1707a78aec52f3e68d542c19bcc861212
NKay <n.klipp@mailbox.org>
2018-06-04 19:55:37 +0200

    Genfifo update

--- genFifo/src/genFifo.cpp
+++ genFifo/src/genFifo.cpp
@@ -3,9 +3,43 @@
 
 
 
-void genFifo::processing(){
 
 
+void genFifo::write_nb(T value){
 
+
+	vec.push_back(value);
+	num++;
+}
+
+T genFifo::read_nb(){
+
+T data = vec.pop();
+num--
+return data;
+
+}
+
+int genFifo::get_ndata(){
+
+	return num;
+}
+
+void genFifo::write_b(T value){
+
+	vec.push_back(value);
+	num++:
+
+}
+
+T genFifo::read_b(){
+
+	T data = vec.pop();
+	num--
+	return data;
+
+}
+
+void genFifo::reset(){
 
 }
--- genFifo/src/genFifo.h
+++ genFifo/src/genFifo.h
@@ -1,16 +1,56 @@
 #include <systemc.h>
+#include <vector>
+
+template<class T>
+class Ififo_read {
+public:
+
+	virtual T read_nb() = 0;
+	virtual T read_b() = 0;
+	virtual void reset() = 0;
+};
+
+class Ififo_write {
+
+	virtual void write_nb(T data) = 0;
+	virtual void write_b(T data) = 0;
+	virtual void reset() = 0;
+};
 
 
-SC_MODULE(genFifo){
+template<class T>
+class genfifo :public sc_module,  public Ififo_write, public Ififo_read
+{
 
 	sc_clock clk;
+	std::vector<T> vec;
+	int num;
 
 
+
+	void write_nb(T data);
+
+	T read_nb();
+
+	int get_ndata();
+
+	void write_b(T data);
+
+	T read_b();
+
+	void reset();
 
 	SC_CTOR(genFifo)//:clk("clk", sc_time(1, SC_SEC))
 	{
-		SC_METHOD(processing);
+		SC_THREAD(write_nb);
+		SC_THREAD(read_nb);
+		SC_METHOD(write_b);
+		SC_METHOD(read_b);
+		SC_METHOD(reset);
+		SC_THREAD(get_ndata);
 		sensitive << clk;
+
+		num = 0;
 
 	}
 
@@ -30,12 +70,24 @@ SC_MODULE(genFifo){
 
 };
 
-SC_MODULE(drain)
+
+SC_MODULE(producer)
+{
+
+
+	SC_CTOR(producer)
+	{
+
+	}
+
+}
+
+SC_MODULE(consumer)
 {
 	sc_in<int> in;
 
-	SC_HAS_PROCESS(drain);
-	drain(sc_module_name n)
+	SC_HAS_PROCESS(consumer);
+	consumer(sc_module_name n)
 	{
 		SC_METHOD(processing);
 		sensitive << in;
