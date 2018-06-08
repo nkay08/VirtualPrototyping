541036f09e3d3edbfc79a1fcd3e4193e80f8453c
NKay08 <n.klipp@mailbox.org>
2018-06-08 15:43:30 +0200

    counter example gtkwave output

--- counter/src/main.cpp
+++ counter/src/main.cpp
@@ -3,13 +3,20 @@
 
 int sc_main(int argc, char * argv[]) {
     
-    sc_signal<double> sig;
+    sc_signal<int> sig;
     
     counter count("count");
     count.out(sig);
     
     drain dr("dr");
     dr.in(sig);
+    
+    // visualise signals in gtkwave
+    
+    
+    sc_trace_file *tf=sc_create_vcd_trace_file("counter");
+    
+    sc_trace(tf, sig, "counter_output");
     
     sc_start(10, SC_SEC);
     
--- counter/src/sim_example.h
+++ counter/src/sim_example.h
@@ -1,18 +1,17 @@
-
 #include <systemc.h>
 
 
 SC_MODULE(counter)
 {
-   sc_out<double> out;
-
+   sc_out<int> out;
+    
    sc_clock clk;
    int cnt;
    double th;
    double int_t;
-
-
-
+   
+    
+    
     SC_HAS_PROCESS(counter);
     counter(sc_module_name n)
     :clk("clk", sc_time(2, SC_SEC))
@@ -23,48 +22,48 @@ SC_MODULE(counter)
         cnt=0.0;
         int_t=0;
         th=2.0;
-
-
+        
+       
     }
-
-
+    
+    
     void processing()
     {
-
+        
         double time=sc_time_stamp().to_seconds();
-
+        
         int_t=0.5*time;
-
+        
         if (int_t>=th)
         {
-            cnt=cnt+1.;
+            cnt=cnt+1;
         }
-
+        
         out.write(cnt);
     }
 };
 
 SC_MODULE(drain)
 {
-    sc_in<double> in;
-
+    sc_in<int> in;
+    
     SC_HAS_PROCESS(drain);
     drain(sc_module_name n)
     {
         SC_METHOD(processing);
         sensitive << in;
         dont_initialize();
-
+        
     }
-
+    
     void processing()
     {
         cout << "Time: " << sc_time_stamp() << endl;
         cout << "Counter value: " <<  in.read();
         cout << endl;
-
+        
     }
-
+    
 };
 
 
