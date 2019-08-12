034c2c767c269e29d94ae5d2b3d3d6fde405d919
nkay08 <n.klipp@mailbox.org>
2019-08-12 16:49:24 +0200

    Update dcmotor_tlm

--- createProject.sh
+++ createProject.sh
@@ -9,10 +9,10 @@ if [ $# -eq 0 ]
 	then
 		echo "Empty String as Paramater"
 	else
-		mkdir $1
+		mkdir -p $1
 		cp -r ./blank/* ./$1
-		mv ./$1/src/blank.h ./$1/src/$1.h 
-		mv ./$1/src/blank.cpp ./$1/src/$1.cpp
+#		mv ./$1/src/blank.h ./$1/src/$1.h
+#		mv ./$1/src/blank.cpp ./$1/src/$1.cpp
 		perl -pi -w -e 's/blank/'$1'/g;' ./$1/src/*
 		perl -pi -w -e 's/blank/'$1'/g;' ./$1/CMakeLists.txt
   fi
